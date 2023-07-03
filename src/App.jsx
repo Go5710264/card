9ab714a3cda4a492f6a02dfafc78e1bbbc076be1
Dima Shirkov <dimashirkov@list.ru>
2023-07-03 22:05:04 +0300

    add Prop-types dependency, card image, removed vite styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import CardWrappre from './components/Card'
+import CardWrapper from './components/Card'
+import Card from 'react-bootstrap/Card'
+
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <>
-      <CardWrappre/>
-    </>
+    <CardWrapper>
+      <Card.Img variant='top' src='../../public/Desert_Electric.jpg'></Card.Img>
+    </CardWrapper>
   )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,22 @@
-// import { Card } from "react-bootstrap";
-
-// const CardWrapper = () => {
-//     return (
-//         <Card style={{width: '18rem'}} bg='primary'>
-//             <Card.Body>
-//                 <Card.Title>Card Title</Card.Title>
-//                 <Card.Text>Привет мир</Card.Text>
-//             </Card.Body>
-//         </Card>
-//     )
-// }
-
-// export default CardWrapper;
-
+import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card'
 
-function CardWrapper() {
+const CardWrapper = ({children}) => {
   return (
-        <Button variant="dark">Go somewhere</Button>
-  );
+    <Card border='info' className='mt-2 ms-2' style={{width: "18rem", padding: '0'}}>
+    {children}
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text className="" >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Card.Text>
+      <Button variant="dark">Go somewhere</Button>
+    </Card.Body>
+  </Card>
+  )
+}
+
+CardWrapper.propTypes = {
+  children: PropTypes.element
 }
 
 export default CardWrapper;
